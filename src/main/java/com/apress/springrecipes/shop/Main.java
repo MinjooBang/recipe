3528c0178ad94d1b07978fff3dc6d847f9a93306
MinjooBang <qkdalswn121@gmail.com>
2020-06-21 12:27:54 +0900

    2.6 끝

--- src/main/java/com/apress/springrecipes/shop/Main.java
+++ src/main/java/com/apress/springrecipes/shop/Main.java
@@ -4,12 +4,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.apress.springrecipes.shop.config.ShopConfiguration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.util.Properties;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(ShopConfiguration.class);
+
+        Resource resource = new ClassPathResource("discounts.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        System.out.println(props);
 
         Product aaa = context.getBean("aaa", Product.class);
         Product cdrw = context.getBean("cdrw", Product.class);
@@ -23,6 +32,7 @@ public class Main {
         ShoppingCart cart2 = context.getBean("shoppingCart", ShoppingCart.class);
         cart2.addItem(dvdrw);
         System.out.println("Shopping cart 2 contains " + cart2.getItems());
+
 
     }
 
